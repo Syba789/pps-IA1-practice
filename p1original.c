48a9c0ea2adbe6b4af46365e1829ee383304e320
Syba789 <95074177+Syba789@users.noreply.github.com>
2022-01-31 06:49:33 +0000

    done

--- p1original.c
+++ p1original.c
@@ -1,2 +1,14 @@
-
+#include<stdio.h>
+int add(int x,int y)
+{
+  int k=y+x;
+  return k;
+}
+int main()
+{
+  int a,b;
+  printf("enter two numbers to add\n");
+  scanf("%d%d",&a,&b);
+  printf("%d+%d=%d",a,b,(a+b));
+  return 0;
 }
